8a06275447e63e9174065d914762699d7d5b10bc
Levjo Cibuku <levjo.cibuku@hotmail.com>
2025-03-21 20:21:29 +0100

    world map done

--- src/main/java/org/example/GamePanel.java
+++ src/main/java/org/example/GamePanel.java
@@ -85,7 +85,7 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
-        tileM.draw8(g2);
+        tileM.draw(g2);
         player.draw(g2);
 
         g2.dispose();
--- src/main/java/tile/TileManager.java
+++ src/main/java/tile/TileManager.java
@@ -1,62 +1,85 @@
 package tile;
 
 import org.example.GamePanel;
-
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class TileManager {
     GamePanel gp;
-    Tile [] tile;
+    Tile[] tile;
+    int[][] mapTileNum;
 
     public TileManager(GamePanel gp) {
         this.gp = gp;
         tile = new Tile[10];
-        getTileimage();
+        mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
+        getTileImage();
+        loadMap();
     }
 
-    public void getTileimage(){
+    private void getTileImage() {
         try {
-            tile[0]= new Tile();
-            tile[0].image= ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/grass.png"));
+            tile[0] = new Tile();
+            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass.png"));
 
-            tile[1]= new Tile();
-            tile[1].image= ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/wall.png"));
+            tile[1] = new Tile();
+            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
 
-            tile[2]= new Tile();
-            tile[2].image= ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/water.png"));
-
+            tile[2] = new Tile();
+            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water.png"));
 
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
-
     }
-    public void draw8(Graphics2D g2) {
-        int col= 0;
-        int row= 0;
-        int x= 0;
-        int y= 0;
 
-        while (col <gp.maxScreenCol && row < gp.maxScreenRow){
-            g2.drawImage(tile[0].image,x,y,gp.tileSize,gp.tileSize,null);
-            col ++;
+    public void loadMap() {
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("maps/world_map_0_1.text");
+             BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+
+            int col = 0;
+            int row = 0;
+
+            while (row < gp.maxScreenRow) {
+                String line = br.readLine();
+                if (line == null) break;
+
+                String[] numbers = line.split(" ");
+                for (col = 0; col < gp.maxScreenCol; col++) {
+                    if (col < numbers.length) {
+                        mapTileNum[col][row] = Integer.parseInt(numbers[col]);
+                    }
+                }
+                row++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void draw(Graphics2D g2) {  // Renamed from draw8
+        int col = 0;
+        int row = 0;
+        int x = 0;
+        int y = 0;
+
+        while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+            int tileNum = mapTileNum[col][row];
+            g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
+            col++;
             x += gp.tileSize;
 
-            if (col == gp.maxScreenCol){
+            if (col == gp.maxScreenCol) {
                 col = 0;
-                x=0;
+                x = 0;
                 row++;
-                y+=gp.tileSize;
+                y += gp.tileSize;
             }
         }
     }
-    }
-
-
-
-
-
-
-
-
+}
